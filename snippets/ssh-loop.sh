66568766032fe50c8956dacef5d6001b7378b0c5
JeffReeves <jeff@binary.run>
2020-12-11 20:02:56 -0700

    Add named parameters
    Script now accepts named paramateres for:
    
    - Inventory files
    
    - Hosts list
    
    - Script to run
    
    - Log file to save to

--- snippets/ssh-loop.sh
+++ snippets/ssh-loop.sh
@@ -3,8 +3,86 @@
 # Purpose: Runs a script on a list of remote servers via ssh 
 #   and logs output to a log file.
 
-SCRIPT_FILENAME=$(script_get_filename "${0}")
-SCRIPT_DIRECTORY=$(script_get_directory "${0}")
+# parse user parameters
+for PARAMETER in "$@"; do 
+    case ${PARAMETER} in 
+
+        -i=*|--inventory=*)
+            INVENTORY_FILE="${PARAMETER#*=}"
+            if [ -z "${INVENTORY_FILE}" ]; then
+                read -rp "${PROMPT} Inventory file of hosts:" INVENTORY_FILE
+            fi
+            shift
+            ;;
+
+        -h=*|--hosts=*)
+            HOSTS="${PARAMETER#*=}"
+            if [ -z "${HOSTS}" ]; then
+                read -rp "${PROMPT} Hosts (space separated):" HOSTS
+            fi
+            shift
+            ;;
+
+        -s=*|--script=*)
+            REMOTE_SCRIPT="${PARAMETER#*=}"
+            if [ -z "${REMOTE_SCRIPT}" ]; then
+                read -rp "${PROMPT} Script to run on remote hosts:" REMOTE_SCRIPT
+            fi
+            shift
+            ;;
+
+        -l=*|--log=*)
+            LOG_FILE="${PARAMETER#*=}"
+            if [ -z "${LOG_FILE}" ]; then
+                read -rp "${PROMPT} File to log output to:" LOG_FILE
+            fi
+            shift
+            ;;
+        
+        *)
+            echo "${ERROR} Parameters passed to ${0} are not valid"
+            echo "${HELP} Acceptable parameters are:"
+            echo "-i=* | --inventory=*  # inventory file of hosts"
+            echo "-h=* | --hosts=*      # hosts to run script on"
+            echo "-s=* | --script=*     # script to run on remote hosts"
+            echo "-l=* | --log=*        # log file to save output to"
+            exit 250
+            ;;
+    esac
+done
+
+# confirm settings passed as parameters
+prompt_confirm_values \
+"INVENTORY_FILE=${INVENTORY_FILE}" \
+"HOSTS=${HOSTS}" \
+"REMOTE_SCRIPT=${REMOTE_SCRIPT}" \
+"LOG_FILE=${LOG_FILE}"
+
+if [ -s "${INVENTORY_FILE}" ]; then 
+    echo "${DEBUG} Invetory file: ${INVENTORY_FILE}"
+fi
+
+# get the directory and filename of this snippet
+SNIPPET_DIRECTORY=$(script_get_directory "${0}")
+SNIPPET_FILENAME=$(script_get_filename "${0}")
+
+# define hosts file
+HOSTS_FILE="${SNIPPET_DIRECTORY}/hosts"
+
+
+
+
+# get remote script, if passed by the user
+if [[ ! -z "${1}" ]]; then
+    USER_INPUT="$@"
+fi
+
+
+# find dotfiles' script directory 
+DOTFILES_DIRECTORY=$(find -O3 "${HOME}" -user ${USER} -type d -name 'dotfiles')
+SCRIPT_DIRECTORY="${DOTFILES_DIRECTORY}/scripts"
+
+SNIPPET_FILENAME=$(script_get_filename "${0}")
 REMOTE_SCRIPT="${1}"
 
 # debugging 
@@ -16,28 +94,14 @@ echo ''
 SCRIPT="${SCRIPT_DIRECTORY}/${SCRIPT_FILENAME}"
 echo "${DEBUG} SCRIPT: ${SCRIPT}"
 
-# LOGFILE='script.log'
-# SERVERLIST=(hostname1 hostname2)
 
-# # create log file if it does not exist
-# if [ ! -f ${LOGFILE} ]; then
-#     echo "[INFO] Creating log file..."
-#     touch ${LOGFILE}
-#     if [ $? -eq 0 ]; then 
-#         echo "[SUCCESS] Log file created at ./${LOGFILE}"
-#     else
-#         echo "[FAILURE] Unable to create log file at ./${LOGFILE}" 
-#         exit 1
-#     fi 
-# fi
-
-# # loop through each server
-# for i in ${SERVERLIST[@]}; do 
-#     SERVER="${i}"
-#     echo "#==[ ${SERVER} ]=========================================="
-#     # pass the script to the server via ssh
-#     ssh ${SERVER} "bash -s" < ./${SCRIPT} # BASH
-#     # ssh ${SERVER} python -u - < ./${SCRIPT} # Python
-#     echo -e "\n"
-# # display the output on STDOUT and tee to log file
-# done | tee -a ${LOGFILE}
+# loop through each server
+for i in ${SERVERLIST[@]}; do 
+    SERVER="${i}"
+    echo "#==[ ${SERVER} ]=========================================="
+    # pass the script to the server via ssh
+    ssh ${SERVER} "bash -s" < ./${SCRIPT} # BASH
+    # ssh ${SERVER} python -u - < ./${SCRIPT} # Python
+    echo -e "\n"
+# display the output on STDOUT and tee to log file
+done | tee -a ${LOGFILE}
