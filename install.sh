552bf7c99d098bf056efe3af908ad13d523a71e2
JeffReeves <jeff@binary.run>
2021-06-06 22:23:55 -0700

    Improve output of install file
    Color output on symlink creation

--- install.sh
+++ install.sh
@@ -41,37 +41,45 @@ fi
 # append dotfiles' .bashrc to ~/.bashrc
 if [ -f "${BASHRC}" ]; then
     if ! grep -q "${DOTFILE_BASHRC}" "${BASHRC}"; then
-        echo "${INFO-[INFO]} ${BASHRC} does not contain ${DOTFILE_BASHRC}"
-		echo "${TASK-[TASK]} Appending ${DOTFILE_BASHRC} ..."
+        echo -e "${INFO-[INFO]} ${BASHRC} does not contain ${DOTFILE_BASHRC}"
+		echo -e "${TASK-[TASK]} Appending ${DOTFILE_BASHRC} ..."
 		echo -e "${COMMAND-[COMMAND]} ${SOURCE_FILES} >> \"${BASHRC}\""
 		echo -e "${SOURCE_FILES}" >> "${BASHRC}"
 		if [ ${?} -eq 0 ]; then 
-			echo "${SUCCESS-[SUCCESS]} ${BASHRC} now contains ${DOTFILE_BASHRC}"
-			echo "${TASK-[TASK]} Source ${BASHRC} file to take effect ..." 
-			echo "${COMMAND-[COMMAND]} source ${BASHRC}"
+			echo -e "${SUCCESS-[SUCCESS]} ${BASHRC} now contains ${DOTFILE_BASHRC}"
+			echo -e "${TASK-[TASK]} Source ${BASHRC} file to take effect ..." 
+			echo -e "${COMMAND-[COMMAND]} source ${BASHRC}"
 			source "${BASHRC}"
 		fi
     else
-		echo "${SUCCESS-[SUCCESS]} ${BASHRC} already contains ${DOTFILE_BASHRC}"
+		echo -e "${SUCCESS-[SUCCESS]} ${BASHRC} already contains ${DOTFILE_BASHRC}"
     fi
 else
-    echo "${ERROR-[ERROR]} ${BASHRC} not found"
-    echo "${HELP-[HELP]} Please create a ${BASHRC} file"
+    echo -e "${ERROR-[ERROR]} ${BASHRC} not found"
+    echo -e "${HELP-[HELP]} Please create a ${BASHRC} file"
 fi
+echo ''
 
 
 # configure git user and email
 GIT_NAME='JeffReeves'
 GIT_EMAIL='jeff@binary.run'
-echo "${TASK} Set global git config username and email ..."
-echo "${COMMAND} git config --global user.name  \"${GIT_NAME}\"" 
-echo "${COMMAND} git config --global user.email \"${GIT_EMAIL}\"" 
+echo -e "${TASK-[TASK]} Set global git config username and email ..."
+echo -e "${COMMAND-[COMMAND]} git config --global user.name  \"${GIT_NAME}\"" 
+echo -e "${COMMAND-[COMMAND]} git config --global user.email \"${GIT_EMAIL}\"" 
 git config --global user.name  "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
-echo "${SUCCESS} Set username and email for git"
+echo -e "${SUCCESS-[SUCCESS]} Set username and email for git"
+echo ''
 
 
 # create links to dotfiles in home directory
+echo -e "${TASK-[TASK]} Create symlinks to dotfiles in home directory ..."
+echo -e "${COMMAND-[COMMAND]} ln -fs \"${CONFIGS}/.gitconfig\" \"${HOME}/.gitconfig\"" 
 ln -fs "${CONFIGS}/.gitconfig" "${HOME}/.gitconfig"
+echo -e "${COMMAND-[COMMAND]} ln -fs \"${CONFIGS}/.tmux.conf\" \"${HOME}/.tmux.conf\"" 
 ln -fs "${CONFIGS}/.tmux.conf" "${HOME}/.tmux.conf"
+echo -e "${COMMAND-[COMMAND]} ln -fs \"${CONFIGS}/.vimrc\"     \"${HOME}/.vimrc\"" 
 ln -fs "${CONFIGS}/.vimrc"     "${HOME}/.vimrc"
+echo -e "${SUCCESS-[SUCCESS]} Created dotfile symlinks"
+echo ''
